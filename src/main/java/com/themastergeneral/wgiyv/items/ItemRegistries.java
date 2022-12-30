253cd0c90c1081f195a2f474fc49f3b695f74eff
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2022-12-29 21:20:29 -0500

    Compile fix for 1.18

--- src/main/java/com/themastergeneral/wgiyv/items/ItemRegistries.java
+++ src/main/java/com/themastergeneral/wgiyv/items/ItemRegistries.java
@@ -30,9 +30,9 @@ package com.themastergeneral.wgiyv.items;
 import com.themastergeneral.wgiyv.WGIYV;
 
 import net.minecraft.world.item.Item;
-import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ItemRegistries {
 public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, WGIYV.MODID);
