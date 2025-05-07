672f39ce8c54a7f40164e4630438c1101d0544b1
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2025-05-07 03:22:00 -0400

    Update copyright year

--- wgiyv-neo/src/main/java/com/themastergeneral/wgiyv/items/ItemRegistries.java
+++ wgiyv-neo/src/main/java/com/themastergeneral/wgiyv/items/ItemRegistries.java
@@ -1,11 +1,11 @@
-/*
-	Project:	WGIYV
+/**
+	Project:	WGIYV 1.21 Neoforge
 	File:		com.themastergeneral.wgiyv.items.ItemRegistries
 	Author:		TheMasterGeneral
 	Website: 	https://github.com/MasterGeneral156/WGIYV
 	License:	MIT License
 
-				Copyright (c) 2024 MasterGeneral156
+				Copyright (c) 2025 MasterGeneral156
 				
 				Permission is hereby granted, free of charge, to any person obtaining a copy
 				of this software and associated documentation files (the "Software"), to deal
